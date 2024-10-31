6f5915ef7744f22482b841c1fc9bb8bfbbf6fc20
ChuHieu <chuhieuqb2003@gmail.com>
2024-10-31 17:19:44 +0700

    CMHCMH

---
+++ GUI/static/js/createMeeting.js
@@ -0,0 +1,156 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("create-meeting-modal");
+  const btn = document.getElementById("create-meeting-btn");
+  const span = document.getElementsByClassName("close")[0];
+  const form = document.getElementById("create-meeting-form");
+  const userId = "zcxsTyXdQZGhk5VLjLPyjw"; // Replace with the actual user ID
+  const accessToken = "your_access_token"; // Replace with the actual access token
+
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  form.onsubmit = async function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const data = {
+      topic: formData.get("topic"),
+      start_time: formData.get("start_time"),
+      duration: parseInt(formData.get("duration")),
+      timezone: formData.get("timezone"),
+      agenda: formData.get("agenda"),
+      type: parseInt(formData.get("type")), // Include the type field
+      invitees: formData
+        .get("invitees")
+        .split(",")
+        .map((email) => email.trim()),
+    };
+
+    try {
+      const response = await fetch("/meetings", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        alert("Cuộc họp đã được tạo thành công!");
+        modal.style.display = "none";
+      } else {
+        const error = await response.json();
+        alert("Lỗi: " + error.detail);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Đã xảy ra lỗi khi tạo cuộc họp.");
+    }
+  };
+  async function fetchUpcomingMeetings() {
+    try {
+      const response = await fetch(`/meetings/users/${userId}/upcoming`, {
+        method: "GET",
+      });
+      if (response.ok) {
+        const meetings = await response.json();
+        console.log(meetings);
+        populateMeetingTable(meetings);
+      } else {
+        const error = await response.json();
+        console.error("Error fetching meetings:", error);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  async function fetchCurrentMeetings() {
+    try {
+      const response = await fetch(`/meetings/users/${userId}/live`, {
+        method: "GET",
+      });
+
+      if (response.ok) {
+        const meetings = await response.json();
+        currentMeetingTable(meetings);
+      } else {
+        const error = await response.json();
+        console.error("Error fetching meetings:", error);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  function populateMeetingTable(meetings) {
+    const tbody = document
+      .getElementById("upcoming-meetings-table")
+      .getElementsByTagName("tbody")[0];
+    tbody.innerHTML = ""; // Clear existing rows
+    meetings.forEach((meeting) => {
+      if(Date.now() > new Date(meeting.start_time).getTime()) return;
+      const row = tbody.insertRow();
+      row.insertCell(0).innerText = meeting.topic;
+      let startTime = new Date(meeting.start_time);
+      startTime.setHours(startTime.getHours() + 7);
+      let formattedDate = startTime.toISOString();
+      row.insertCell(1).innerText = formattedDate;
+      row.insertCell(2).innerText = meeting.duration;
+      row.insertCell(
+        3
+      ).innerHTML = `<button onclick="manageMeeting('${meeting.id}')">Quản lý</button>`;
+      row.insertCell(
+        4
+      ).innerHTML = `<button onclick="joinMeeting('${meeting.join_url}')">Tham gia</button>`;
+    });
+  }
+
+  function currentMeetingTable(meetings) {
+    const tbody = document
+      .getElementById("current-meetings-table")
+      .getElementsByTagName("tbody")[0];
+    tbody.innerHTML = ""; // Clear existing rows
+    const now = new Date();
+    meetings.forEach((meeting) => {
+      const row = tbody.insertRow();
+      row.insertCell(0).innerText = meeting.topic;
+      let startTime = new Date(meeting.start_time);
+      startTime.setHours(startTime.getHours() + 7);
+      let formattedDate = startTime.toISOString();
+      row.insertCell(1).innerText = formattedDate;
+      row.insertCell(2).innerText = meeting.duration;
+      row.insertCell(
+        3
+      ).innerHTML = `<button onclick="manageMeeting('${meeting.id}')">Quản lý</button>`;
+      row.insertCell(
+        4
+      ).innerHTML = `<button onclick="joinMeeting('${meeting.join_url}')">Tham gia</button>`;
+    });
+  }
+
+  fetchUpcomingMeetings();
+  fetchCurrentMeetings();
+});
+
+function manageMeeting(meetingId) {
+  alert(`Quản lý cuộc họp: ${meetingId}`);
+  // Add your management logic here
+}
+
+function joinMeeting(joinUrl) {
+  // redirect to the meeting page by another tab
+  window.open(joinUrl, "_blank");
+}
---
+++ webhooks/webhook.py
@@ -0,0 +1,76 @@
+from fastapi import APIRouter, Request, HTTPException, Header
+import json
+import secrets
+import hmac
+import hashlib
+import os
+from dotenv import load_dotenv
+import datetime
+import paho.mqtt.client as mqtt
+
+load_dotenv()
+
+webhook_router = APIRouter()
+
+MQTT_BROKER = os.environ.get('MQTT_BROKER')  
+MQTT_PORT = int(os.environ.get("MQTT_PORT"))               
+MQTT_USERNAME = os.environ.get('MQTT_USERNAME')  
+MQTT_PASSWORD = os.environ.get('MQTT_PASSWORD') 
+print(MQTT_BROKER, MQTT_PORT, MQTT_USERNAME, MQTT_PASSWORD)
+client = mqtt.Client()
+
+client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+client.connect(MQTT_BROKER, MQTT_PORT, keepalive=60)
+print(f"Đã kết nối tới MQTT broker: {MQTT_BROKER}")
+
+ZOOM_SECRET_TOKEN = os.environ.get("ZOOM_WEBHOOK_SECRET_TOKEN")
+
+@webhook_router.post("/webhook")
+async def webhook(request: Request):
+    # print(ZOOM_SECRET_TOKEN)
+    headers = dict(request.headers)
+    body = await request.json()
+    # print(headers)
+    print("Body", body)
+
+    if 'payload' in body and 'plainToken' in body['payload']: # Dùng để validated url trên link zoom webhook
+        secret_token = ZOOM_SECRET_TOKEN.encode("utf-8")
+        plaintoken = body['payload']['plainToken']
+        mess = plaintoken.encode("utf-8")
+        has = hmac.new(secret_token, mess, hashlib.sha256).digest()
+        hexmessage = has.hex()
+
+        response = {
+            'message': {
+                'plainToken': plaintoken,
+                'encryptedToken': hexmessage
+            }
+        }
+        print(response['message'])
+        return response['message']
+    
+    payload = body.get('payload')
+    event = body.get('event')
+    print(payload)
+    print(event)
+    object_payload = payload['object']
+    print(object_payload)
+    participant = object_payload['participant']
+    name = participant['user_name']
+    
+    if event == 'meeting.participant_joined':
+        topic = 'zoom/participant/joined'
+        if payload:
+            joined_time = participant['join_time']
+            timestamp = datetime.datetime.fromisoformat(joined_time.replace("Z", "+00:00"))
+            utc_plus_7 = timestamp + datetime.timedelta(hours=7)
+            formatted_timestamp = utc_plus_7.strftime("[%d-%m-%Y %H:%M:%S]")
+            send_data = {"message": formatted_timestamp + " " + name + " đã tham gia cuộc họp"}
+            client.publish(topic, json.dumps(send_data))
+            print(f"Đã gửi dữ liệu tới {topic}: {payload}")
+    elif event == 'meeting.participant_left':
+        topic = 'zoom/participant/left'
+        if payload:
+            client.publish(topic, json.dumps(payload))
+            print(f"Đã gửi dữ liệu tới {topic}: {payload}")
